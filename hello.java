da289ba51b5d0772a7aace5856b18821b7353e12
Dileep9123 <121809810+Dileep9123@users.noreply.github.com>
2023-08-07 19:41:14 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,4 +1,7 @@
-public class Dileep {
-    public statdsjvchjdvdvcvd
+public class GitHub {
+    public static void main(String args[])
+    {
+        System.out.println("Hello Worls\n");
+    }
 
 }
